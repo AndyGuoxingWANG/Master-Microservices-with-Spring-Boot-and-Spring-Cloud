280ab56ba19eae94cc6d6960047acd1976d7c99c
Andy Guoxing Wang <wang.guox@northeastern.edu>
2021-06-01 19:47:39 -0400

    2nd chapter

--- src/main/java/com/in28minutes/res/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/in28minutes/res/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.in28minutes.res.webservices.restfulwebservices.exception;
 
 import com.in28minutes.res.webservices.restfulwebservices.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +27,12 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     public final ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) throws Exception {
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+            ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "Validation Failed by Andy", ex.getBindingResult().toString());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/in28minutes/res/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/res/webservices/restfulwebservices/user/UserResource.java
@@ -1,10 +1,15 @@
 package com.in28minutes.res.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
@@ -21,16 +26,19 @@ public class UserResource {
    }
 
    @GetMapping("/users/{id}")
-   public User retrieveUser(@PathVariable int id){
+   public EntityModel<User> retrieveUser(@PathVariable int id){
       User user = service.findOne(id);
       if (user == null){
          throw new UserNotFoundException("id-" + id);
       }
-      return user;
+      EntityModel<User> model = EntityModel.of(user);
+      WebMvcLinkBuilder linkToUsers =
+              linkTo(methodOn(this.getClass()).retrieveAllUsers());
+      return model;
    }
 
    @PostMapping("/users")
-   public ResponseEntity<Object> createUser(@RequestBody User user){
+   public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
       User savedUser = service.save(user);
       URI location = ServletUriComponentsBuilder
               .fromCurrentRequest()
